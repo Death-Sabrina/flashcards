a5a3edae61922c5c2273a59db236db39860ab0da
Sabrina Parmesano Nassife <149083448+Death-Sabrina@users.noreply.github.com>
2024-09-27 07:46:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'me!',
+    'Quem eu sou?',
+    'I am a trans non-binary writer S2'
+)
+
+criaCartao(
+    'eu!',
+    'o que eu faço?',
+    'eu escrevo historias e textos melancolicos sobe os meus sentimentos'
+)
+
+criaCartao(
+    'mi!',
+    'o que eu gostaria?',
+    'Bu toil leam a bhith air mo làimhseachadh mar bhalach... riochdairean fireann agus a h-uile càil!'
+)
+
+criaCartao(
+    'egó!',
+    'do que eu preciso?',
+    'βοήθειαβοήθειαβοήθειαβοήθειαβοήθειαβοήθειαβοήθειαβ'
+)
